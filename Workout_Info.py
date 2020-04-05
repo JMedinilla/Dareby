e03709627dcefd5f224c93242a703e0d6a975156
Javier Medinilla <javimedinilla@gmail.com>
2020-04-05 22:13:37 +0200

    Dev (#3)
    * Scripts to find and download workouts
    
    * Some stupid format
    
    * What the actual fuck, really? (#2)

--- Workout_Info.py
+++ Workout_Info.py
@@ -53,7 +53,7 @@ def retrieveWorkoutInfo(workoutUrl):
     except:
         bsExtraCredit = ""
 
-    workoutUrl = workoutUrl
+
     workoutName = bsExercises[bsExercises.find("ts/")+3:bsExercises.find(".")]
     workoutExercises = "https://darebee.com" + bsExercises
     workoutMuscles = "https://darebee.com" + bsMuscles
